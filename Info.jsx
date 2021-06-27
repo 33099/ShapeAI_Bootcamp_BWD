aad31d646b8ebed2961f82ef654b18ab0265f641
Divya Sharma <73958061+33099@users.noreply.github.com>
2021-06-27 19:41:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Java Script and React.js</h1>
+      <p>
+        {" "}
+        A basic web dev React Js Bootcamp. This was an amazing bootcamp taken up
+        by Shaurya Sinha.
+      </p>
+    </div>
+  );
+}
+export default Info;
